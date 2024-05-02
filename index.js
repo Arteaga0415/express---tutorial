cd7d60296ce8ce4742a0b1d625d034a080a85433
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-05-02 12:27:37 -0500

    feat: serving text, data and files

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+const items = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+//send text
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+//send objects 
+app.get('/object', (req, res) => {
+  res.json(items)
+})
+//serve html files
+app.use(express.static('public'));
+app.get('/example', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'example.html'))
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log(`Server is running on http://localhost:${port}`);
 })
