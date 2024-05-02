8d2297cccb8e48f3cb4518c4847b39aa2e4a216d
Sebastian Arteaga Ronga <sarteaga0415@gmail.com>
2024-05-02 12:41:21 -0500

    feat: basic turtorial finished

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/example', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'example.html'))
 })
 
-app.use('/static', express.static(path.join(__dirname, 'public', 'example.html')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
